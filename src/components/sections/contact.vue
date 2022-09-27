0daf1c7317c7bc95d85b0a0e1b2502252812ace6
AkosoCodes <74971935+AkosoCodes@users.noreply.github.com>
2022-09-27 13:22:43 +0200

    Added Contact section

--- src/components/sections/contact.vue
+++ src/components/sections/contact.vue
@@ -2,6 +2,20 @@
 
     <Heading heading="Contact" headingNum="03"></Heading>
 
+    <div class=" w-2/3 flex justify-center flex-col mt-[50px] mx-auto select-none mb-[50px]">
+
+        <h1 class="text-accent text-[40px] font-bold text-center">Get in touch.</h1>
+        <p class="text-main text-center text-[20px]">
+            I am currently looking for any new opportunities, be it projects, interships or junior positions. Whether you have a question, proposal or just want to say hi, my inbox is always open! :)
+        </p>
+
+		<!-- Learn More Button -->
+		<button class="mx-auto w-150px h-50px bg-accent text-main my-5 bg-opacity-70 hover:bg-main hover:bg-opacity-30 hover:text-accent">
+			Learn More
+		</button>
+    
+    </div>
+
 </template>
 
 <script>
