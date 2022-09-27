8ebc2c3b420f1554f16ac36b43e84f8b35a28a71
AkosoCodes <74971935+AkosoCodes@users.noreply.github.com>
2022-09-27 09:08:21 +0200

    🌠| Made Responsive Fixed and finished all bugs containing to the responsiveness of the Hero Section.

--- src/components/sections/hero.vue
+++ src/components/sections/hero.vue
@@ -1,42 +1,31 @@
 <template>
 
-	<div class="mx-auto my-[5vw] w-2/3 flex flex-col text-main">
+	<!-- Hero Section Container -->
+	<div class="nav-sm:w-[80%] ssm:w-[85%] mx-auto my-[5vw] w-2/3 flex flex-col text-main">
 
+		<!-- TypeWriter -->
 		<TypeWriter></TypeWriter>
 
-
+		<!-- Header -->
 		<p class="flex flex-col select-none font-extrabold text-main text-lg leading-none h-[10vw] ssm:h-[50px] mt-[5px]">
-			 <a class="ssm:text-[40px] select-none text-xl text-main hover:text-accent" href="">I'm Andrija.</a>
+			<span class="ssm:text-[40px] select-none text-xl text-main ">
+				I'm 
+				<span class="hover:text-accent">Andrija.</span>
+			</span>
 		</p>
 
+		<!-- Description -->
 		<p class="text-base ssm:text-base-sm">
 			I am a software engineering student, dedicated to learning how to build
 			exceptional digital experiences and exploring different technologies.
 		</p>
 
-		<button class="w-150px h-50px bg-accent text-main my-5 bg-opacity-70 hover:bg-main hover:bg-opacity-30 hover:text-accent">Learn More</button>
+		<!-- Learn More Button -->
+		<button class="w-150px h-50px bg-accent text-main my-5 bg-opacity-70 hover:bg-main hover:bg-opacity-30 hover:text-accent">
+			Learn More
+		</button>
+
 	</div>
-
-
-	<!-- <div class="mx-auto my-[10vw] w-2/3 flex-col text-white ">
-
-		<TypeWriter></TypeWriter>
-
-		<p class="select-none text-xl font-extrabold text-main">
-			
-			My name is 
-			<a class="select-none text-main hover:text-accent" href="">Andrija Stankovic!</a>
-		</p>
-
-		<p class="text-base ">
-			I’m a software engineer student, dedicated to learning how to build
-			exceptional digital experiences. Currently, I’m enjoying the process of discovering new technologies and
-			how to implement them in my projects.
-		</p>
-
-
-
-	</div> -->
 
 </template>
 
@@ -48,7 +37,5 @@
 		name: "hero",
 		components: { TypeWriter }
 	}
-
-	window.getSelection().toString();
 
 </script>
