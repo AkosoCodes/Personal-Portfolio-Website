d2a298b73733806adccfc03e88fd9c7f56fa7e40
AkosoCodes <74971935+AkosoCodes@users.noreply.github.com>
2022-09-29 11:33:00 +0200

    Added Call Button

--- src/components/sections/contact.vue
+++ src/components/sections/contact.vue
@@ -2,19 +2,27 @@
 
     <Heading heading="Contact" headingNum="03"></Heading>
 
-    <div class=" w-2/3 flex justify-center flex-col mt-[50px] mx-auto select-none mb-[50px]">
+    <div class="nav-sm:w-[80%] ssm:w-[85%] w-2/3 flex justify-center flex-col mt-[50px] mx-auto select-none mb-[50px]">
 
         <h1 class="text-accent text-[40px] font-bold text-center">Get in touch.</h1>
-        <p class="text-main text-center text-[20px]">
+        <p class="text-main text-center text-base ssm:text-base-sm">
             I am currently looking for any new opportunities, be it projects, interships or junior positions. Whether you have a question, proposal or just want to say hi, my inbox is always open! :)
         </p>
 
 		<!-- Learn More Button -->
-		<a href="mailto:andrijas0721@gmail.com" class="mx-auto"><button class="mx-auto w-150px h-50px bg-accent text-main my-5 bg-opacity-70 hover:bg-main hover:bg-opacity-30 hover:text-accent">
-			Send Message
-		</button></a>
+        <div class="mx-auto">
+            <a href="mailto:andrijas0721@gmail.com" class=""><button class=" w-150px h-50px bg-accent text-main my-5 bg-opacity-70 hover:bg-main hover:bg-opacity-30 hover:text-accent">
+                Send Email
+            </button></a>
+    
+            <a href="tel:+381695566136" class="ml-[10px]"><button class="w-50px h-50px bg-accent text-main my-5 bg-opacity-70 hover:bg-main hover:bg-opacity-30 hover:text-accent">
+                Call
+            </button></a>
+        </div>
     
     </div>
+
+
 
 </template>
 
