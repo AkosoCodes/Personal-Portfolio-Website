d1c453cefe956b619b09f60078ed73cd0f4bded4
AkosoCodes <74971935+AkosoCodes@users.noreply.github.com>
2022-09-29 12:03:01 +0200

    Added Routing

--- src/components/heading.vue
+++ src/components/heading.vue
@@ -1,13 +1,13 @@
 <template>
 
     <!-- Heading Component -->
-    <div class="heading-md:w-[85%] md:w-[70%] ssm:hidden flex flex-row justify-between w-2/3 h-auto mx-auto select-none">
+    <div :id=tempID class="heading-md:w-[85%] md:w-[70%] ssm:hidden flex flex-row justify-between w-2/3 h-auto mx-auto select-none">
         <h1 class="heading-md:text-[6vw] md:text-[5vw] text-main hover:text-accent"><span class="text-accent">{{headingNum}}. </span>{{heading}}</h1>
         <div class="heading-md:w-[60%] md:w-3/5 h-0.5 my-auto  bg-main"></div>
     </div>
 
-    <div class="heading-md:w-[85%] md:w-[70%] ssm:block hidden flex flex-row justify-left w-2/3 h-auto mx-auto my-[20px] select-none">
-        <h1 class="text-[35px]  text-main hover:text-accent"><span class="text-accent">{{headingNum}}. </span>{{heading}}</h1>
+    <div :id=tempID class="heading-md:w-[85%] md:w-[70%] ssm:block hidden flex flex-row justify-left w-2/3 h-auto mx-auto my-[20px] select-none">
+        <h1 class="text-[35px]  text-main hover:text-accent"><span class="text-accent">{{headingNum}}. {{tempID}}</span>{{heading}}</h1>
     </div>
 
 
@@ -23,6 +23,10 @@
                 required: true
             },
             heading: {
+                type: String,
+                required: true
+            },
+            tempID: {
                 type: String,
                 required: true
             }
